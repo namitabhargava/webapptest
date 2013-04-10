02f3f9ed7ff45f6ca56c8093618c4fef38af0975
namitabhargava <bhargavanams@gmail.com>
2013-04-10 16:39:05 -0300

    Update Person.java

--- src/com/proquest/interview/phonebook/Person.java
+++ src/com/proquest/interview/phonebook/Person.java
@@ -1,7 +1,37 @@
-package com.proquest.interview.phonebook;
-
+//Namita Bhargava
 public class Person {
 	public String name;
 	public String phoneNumber;
 	public String address;
+	//Constructor
+	public Person(String name,String phoneNumber, String address ){
+		this.name=name;
+		this.phoneNumber=phoneNumber;
+		this.address=address;
+	}
+	//Getters and Setters
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
 }
